c7351b0ad26db73368389caf6b5ec2d08d9e0c75
Your Name <you@example.com>
2017-07-12 14:18:13 -0400

    added in-class practice problems

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -10,6 +10,40 @@ namespace Dictionaries
     {
         static void Main(string[] args)
         {
+            //Dictionary<string, string> snackMachine = new Dictionary<string, string>()
+            //{
+            //    { "A1", "Twix" }, // { "key", "value" } (key value pair)
+            //    { "A2", "Twizzlers" },
+            //    { "A3", "Reeses" },
+            //    { "A4", "Sourpatch Kids" }
+            //};
+            //foreach (KeyValuePair<string, string> item in snackMachine)
+            //{
+            //    Console.WriteLine(item); // prints out all key value pairs
+            //}
+
+            //foreach (var item in snackMachine) // not best practice to use this because you don't know what data type you're using
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+            //drinkMachine.Add(10, "Ginger Ale"); // using add method
+            //drinkMachine.Add(11, "Bottled Water");
+            //drinkMachine.Add(12, "Orange Juice");
+            //drinkMachine.Add(13, "Fruit Punch");
+            //drinkMachine.Add(14, "Mountain Dew Code Red");
+
+            ////Console.WriteLine(drinkMachine.Count); // using count method to give number of entries
+
+            //foreach (KeyValuePair<int,string> drink in drinkMachine)
+            //{
+            //    Console.WriteLine(drink.Key); // drink.Key will return all the keys
+            //    Console.WriteLine(drink.Value); //drink.Value will return all the values
+            //}
+
+
+
         }
     }
 }
